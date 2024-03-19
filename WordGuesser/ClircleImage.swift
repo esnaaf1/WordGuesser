1023591279d410f33f2f2e51ae554c89346bd627
esnaaf1 <farshad.esnaashari@gmail.com>
2024-03-19 13:52:29 -0500

    Initial Commit

---
+++ WordGuesser/ClircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  ClircleImage.swift
+//  WordGuesser
+//
+//  Created by Farshad Esnaashari on 3/19/24.
+//
+
+import SwiftUI
+
+struct ClircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClircleImage()
+}
---
+++ WordGuesser/WordList.swift
@@ -0,0 +1,8 @@
+//
+//  WordList.swift
+//  WordGuesser
+//
+//  Created by Farshad Esnaashari on 3/19/24.
+//
+
+import Foundation
