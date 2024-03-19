4f675b9f17c2771967488391657409cbeb9f8207
esnaaf1 <farshad.esnaashari@gmail.com>
2024-03-19 10:10:04 -0500

    Initial Commit

---
+++ WordGuesser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGuesser
+//
+//  Created by Farshad Esnaashari on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGuesser/WordGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGuesserApp.swift
+//  WordGuesser
+//
+//  Created by Farshad Esnaashari on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
